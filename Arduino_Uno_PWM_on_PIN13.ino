2cffb02ab86b9f304e82cbbd5c0b910ff0d25911
Ali Mirjamali <ali.mirjamali@gmail.com>
2015-01-25 00:22:56 +0330

    Adding main sketch file and updating the README.md

---
+++ Arduino_Uno_PWM_on_PIN13.ino
@@ -0,0 +1,35 @@
+/******************************************************************************/
+/*                                                                            */
+/*  Emulating Pulse Width Modulation on Arduino Uno R3 onboard LED on PIN 13  */
+/*                                                                            */
+/* This program modulates a sine wave with PWM to fade in/out the onboard LED */
+/* on Arduino UNO ATmega328P's PB5 PIN which can not be used for hardware PWM */
+/*                                                                            */
+/******************************************************************************/
+
+#include <math.h>
+
+// 3600 steps per cycle
+float myStep=0.1;
+// Calculating Pi (3.14159 etc.) the correct way
+float Pi=4.0*atan(1.0);
+
+void setup() 
+{                
+  pinMode(13, OUTPUT);
+}
+
+void loop() 
+{
+  for (float a=0.0; a<360.0; a+=myStep)
+  {
+    // Calculatign delay for to emulate a sine wave
+    int myDelay=int(abs(sin(a*Pi/180.0)*950.0)+25);
+    // Turn the LED on
+    digitalWrite(13, HIGH);
+    delayMicroseconds(1000-myDelay);
+    // set the LED off
+    digitalWrite(13, LOW);
+    delayMicroseconds(myDelay);
+  }
+}
--- README.md
+++ README.md
@@ -1,2 +1,4 @@
-# Arduino_Uno_PWM_on_PIN13
-Simple dirty software PWM emulation on Arduino PIN 13 (The onboard LED)
+## Emulating Pulse Width Modulation on Arduino Uno R3 onboard LED on PIN 13
+
+This program modulates a sine wave with PWM to fade in/out the onboard LED on Arduino UNO ATmega328P's PB5 PIN which can not be used with hardware PWM
+
